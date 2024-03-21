4d2bbeaca365cf2cc875465e1378031d2bac4f6f
Adelhabb <162194923+Adelhabb@users.noreply.github.com>
2024-03-21 11:52:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,29 @@ provider "proxmox" {
   pm_password    = adminprox
   pm_tls_insecure = true
 }
+resource "proxmox_vm_qemu" "ubuntu_vm" {
+  name            = "ubuntu-vm"
+  clone           = var.clone_template
+  target_node     = var.target_node
+  target_storage  = var.target_storage
+  full_clone      = true
+  network {
+    model   = "virtio"
+    bridge  = var.bridge
+  }
+  disk {
+    id       = 0
+    storage  = var.target_storage
+    size     = var.disk_size
+  }
+  os_type = var.os_type
+}
+variable "pm_api_url" {}
+variable "pm_user" {}
+variable "pm_password" {}
+variable "clone_template" {}
+variable "target_node" {}
+variable "target_storage" {}
+variable "bridge" {}
+variable "disk_size" {}
+variable "os_type" {}
