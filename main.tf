47dad28606c6573ec11164b9d7597942f1aba6b3
Adelhabb <162194923+Adelhabb@users.noreply.github.com>
2024-03-28 17:47:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,26 @@
 provider "proxmox" {
-  pm_api_url     = https://192.168.217.128:8006/#v1:0:=qemu%2F100:4:::::8::
-  pm_user        = root
-  pm_password    = adminprox
-  pm_tls_insecure = true
+  pm_api_url       = var.pm_api_url
+  pm_user          = var.pm_user
+  pm_password      = var.pm_password
+  pm_tls_insecure  = true
 }
 resource "proxmox_vm_qemu" "ubuntu_vm" {
-  name            = "ubuntu-vm"
-  clone           = var.clone_template
-  target_node     = var.target_node
-  target_storage  = var.target_storage
-  full_clone      = true
+  name          = "ubuntu-vm"
+  clone         = "template"  # Clone from a template
+  target_node   = var.target_node
+  target_storage = var.target_storage
+  full_clone    = true
+
   network {
     model   = "virtio"
     bridge  = var.bridge
   }
+
   disk {
     id       = 0
     storage  = var.target_storage
     size     = var.disk_size
   }
+
   os_type = var.os_type
 }
-variable "pm_api_url" {}
-variable "pm_user" {}
-variable "pm_password" {}
-variable "clone_template" {}
-variable "target_node" {}
-variable "target_storage" {}
-variable "bridge" {}
-variable "disk_size" {}
-variable "os_type" {}
